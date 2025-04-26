12168b5c7effbd038fc6774ba0b05805e5bcfccf
Vishwash Khandal <65219732+Believekhandal@users.noreply.github.com>
2025-04-26 20:52:04 +0530

    Revert "initial commoit"
    This reverts commit da49f2ccae8d8ea938193f83a4ec37563c9c5307.

--- dockerdemo/src/main/java/com/dockerdemo/dockerdemo/controller/HelloWorldController.java
+++ dockerdemo/src/main/java/com/dockerdemo/dockerdemo/controller/HelloWorldController.java
@@ -1,18 +1,15 @@
 package com.dockerdemo.dockerdemo.controller;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
-@Controller
-@RequestMapping("/api/v1")
+@RestController
+@RequestMapping("/api") // Optional base URL
 public class HelloWorldController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") // Endpoint URL: /api/hello
     public String sayHello() {
-        log.info("Request received.....");
-        return "home"; // It will look for home.html under src/main/resources/templates/
+        return "Hello, World!";
     }
 }
